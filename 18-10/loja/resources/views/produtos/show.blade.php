86a167062822297daa7ac1f8edb02cdf371f6b4a
RAFAEL MARTINS SOARES <rafael.msoares5@senacsp.edu.br>
2022-10-18 22:10:11 -0300

    ACL(3)

---
+++ 18-10/loja/resources/views/produtos/show.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2> Detalhes do Produto</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('produtos.index') }}"> Voltar</a>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Nome:</strong>
+
+            {{ $prod->nome }}
+
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Descrição:</strong>
+
+            {{ $prod->descricao }}
+
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Preco:</strong>
+
+            {{ $prod->preco }}
+
+        </div>
+    </div>
+
+</div>
+
+@endsection
